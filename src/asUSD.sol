61853b65201416e6d9da4eb17fec5abf2fe9a76c
Roman Böhringer <r.boehringer@opencore.ch>
2023-09-07 16:01:05 +0200

    Withdrawing carry

--- src/asUSD.sol
+++ src/asUSD.sol
@@ -13,6 +13,11 @@ contract asUSD is ERC20, Ownable2Step {
                                  STATE
     //////////////////////////////////////////////////////////////*/
     IasUSDFactory public factory;
+
+    /*//////////////////////////////////////////////////////////////
+                                 EVENTS
+    //////////////////////////////////////////////////////////////*/
+    event CarryWithdrawal(uint256 amount);
     
     /// @notice Initiates CSR on main- and testnet
     /// @param _name Name of the token
@@ -52,5 +57,27 @@ contract asUSD is ERC20, Ownable2Step {
         require(returnCode == 0, "Error when redeeming"); // 0 on success: https://docs.compound.finance/v2/ctokens/#redeem-underlying
         _burn(msg.sender, _amount);
         SafeERC20.safeTransfer(note, msg.sender, _amount);
+    }
+
+    /// @notice Withdraw the cNOTE interest that accrued, only callable by the owner.
+    /// @param _amount Amount of cNOTE to withdraw. 0 for withdrawing the maximum possible amount
+    /// @dev The function checks that the owner does not withdraw too much cNOTE, i.e. that a 1:1 NOTE:asUSD exchange rate can be maintained after the withdrawal
+    function withdrawCarry(uint256 _amount) external onlyOwner {
+        address cNote = factory.note();
+        uint256 exchangeRate = CTokenInterface(cNote).exchangeRateCurrent(); // Scaled by 1 * 10^(18 - 8 + Underlying Token Decimals), i.e. 10^(28) in our case
+        // The amount of cNOTE the contract has to hold (based on the current exchange rate which is always increasing) such that it is always possible to receive 1 NOTE when burning 1 asUSD
+        uint256 cNoteRequiredForSupply = (totalSupply() * 1e28 + 1) / exchangeRate; // _amount has 18 decimals, we scale it by 10^28 such that it still has 18 decimals after the division. + 1 such that the calculation rounds up
+        uint maximumWithdrawable = cNoteRequiredForSupply - CTokenInterface(cNote).balanceOf(address(this));
+        if (_amount == 0) {
+            _amount = maximumWithdrawable;
+        } else {
+            require(_amount <= maximumWithdrawable, "Too many tokens requested");
+        }
+        // Technically, _amount can still be 0 at this point, which would make the following two calls unnecessary.
+        // But we do not handle this case specifically, as the only consequence is that the owner wastes a bit of gas when there is nothing to withdraw
+        uint256 returnCode = CErc20Interface(cNote).redeem(_amount);
+        require(returnCode == 0, "Error when redeeming"); // 0 on success: https://docs.compound.finance/v2/ctokens/#redeem
+        SafeERC20.safeTransfer(IERC20(cNote), msg.sender, _amount);
+        emit CarryWithdrawal(_amount);
     }
 }
